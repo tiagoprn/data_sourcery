5ef3211549bad35362bccc084fb6d64bd49f9fc9
tiago.paranhos <tiagoprn@gmail.com>
2019-06-03 17:32:15 -0300

    Creating package to upload to PyPI.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data_sourcery",
+    version="0.5",
+    author="Tiago Paranhos Lima",
+    author_email="tiago@tiagoprnl.me",
+    description="Get data (e.g. images) from sources.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiagoprn/data_sourcery/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
